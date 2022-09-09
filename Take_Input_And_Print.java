50cf4719be86373e5e91a2fd9a90c67ba9be7455
ParepalliSriram <96734287+ParepalliSriram@users.noreply.github.com>
2022-09-09 12:06:32 +0530

    import java.util.Scanner; class First {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         String c;         c=sc.nextLine();         System.out.println(c);         sc.close();     } }

---
+++ Take_Input_And_Print.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+class First
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        String c;
+        c=sc.nextLine();
+        System.out.println(c);
+        sc.close();
+    }
+}
