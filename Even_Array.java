0aa5a0088ca278e36837726ce4cf4068f7f2b8e6
ParepalliSriram <96734287+ParepalliSriram@users.noreply.github.com>
2022-09-13 18:52:50 +0530

    import java.util.Scanner; class First {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int a=sc.nextInt();         int[] arr1=new int[a];         int[] arr2=new int[100];         int i,j,k,l=0,count=0,avg;         for(i=0;i<a;i++)         {             arr1[i]=sc.nextInt();         }         for(i=0;i<a;i++)         {             if(arr1[i]%2!=0)             {                 count+=1;                 System.out.print("False");                 break;             }         }         if(count==0)         {             System.out.print("True");         }     } }

---
+++ Even_Array.java
@@ -0,0 +1,29 @@
+import java.util.Scanner;
+class First
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int[] arr1=new int[a];
+        int[] arr2=new int[100];
+        int i,j,k,l=0,count=0,avg;
+        for(i=0;i<a;i++)
+        {
+            arr1[i]=sc.nextInt();
+        }
+        for(i=0;i<a;i++)
+        {
+            if(arr1[i]%2!=0)
+            {
+                count+=1;
+                System.out.print("False");
+                break;
+            }
+        }
+        if(count==0)
+        {
+            System.out.print("True");
+        }
+    }
+}
