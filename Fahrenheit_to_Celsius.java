4e56bb9ebb05633dec97a4aa49a5b24b17c16982
ParepalliSriram <96734287+ParepalliSriram@users.noreply.github.com>
2022-09-15 08:01:14 +0530

    import java.util.Scanner; class First {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         float a=sc.nextFloat();         System.out.printf("%.2f",((a-32)*(0.5555555555555556)));     } }

---
+++ Fahrenheit_to_Celsius.java
@@ -0,0 +1,10 @@
+import java.util.Scanner;
+class First
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        float a=sc.nextFloat();
+        System.out.printf("%.2f",((a-32)*(0.5555555555555556)));
+    }
+}
