81187997807f32bd6e724e972e567b860ffe0155
ParepalliSriram <96734287+ParepalliSriram@users.noreply.github.com>
2022-09-18 07:23:41 +0530

    import java.util.Scanner; class First {     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int a=sc.nextInt();         int[] arr=new int[a];         int[] arr2=new int[100];         int i,j,k=0,l;         for(i=0;i<a;i++)         {             arr[i]=sc.nextInt();         }         int b=sc.nextInt();         int c=sc.nextInt();         for(i=0;i<a;i++)         {             if(arr[i]>=b && arr[i]<=c)             {                 continue;             }             else             {                 arr2[k++]=arr[i];             }         }         if(k==0)         {             System.out.print("-1");         }         else         {             for(i=0;i<k;i++)             {                 System.out.print(arr2[i]+" ");             }         }     } }

---
+++ elements_not_in_between_A_and_B.java
@@ -0,0 +1,40 @@
+import java.util.Scanner;
+class First
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int[] arr=new int[a];
+        int[] arr2=new int[100];
+        int i,j,k=0,l;
+        for(i=0;i<a;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        int b=sc.nextInt();
+        int c=sc.nextInt();
+        for(i=0;i<a;i++)
+        {
+            if(arr[i]>=b && arr[i]<=c)
+            {
+                continue;
+            }
+            else
+            {
+                arr2[k++]=arr[i];
+            }
+        }
+        if(k==0)
+        {
+            System.out.print("-1");
+        }
+        else
+        {
+            for(i=0;i<k;i++)
+            {
+                System.out.print(arr2[i]+" ");
+            }
+        }
+    }
+}
