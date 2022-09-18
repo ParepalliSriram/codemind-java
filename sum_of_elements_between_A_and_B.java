b2afb9c44a532a8603338f554e827999a2f3b2c4
ParepalliSriram <96734287+ParepalliSriram@users.noreply.github.com>
2022-09-18 08:49:21 +0530

    import java.util.Scanner; class First {     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int a=sc.nextInt();         int[] arr=new int[a];         int[] arr2=new int[100];         int i,j,k=0,l=0,m=0,n=0,p=0;         for(i=0;i<a;i++)         {             arr[i]=sc.nextInt();         }         int b=sc.nextInt();         int c=sc.nextInt();         for(i=0;i<a;i++)         {             if(arr[i]>=b && arr[i]<=c)             {                 arr2[m++]=arr[i];             }         }         if(m==0)         {             System.out.print("-1");         }         else         {             for(i=0;i<m;i++)             {                 p+=arr2[i];             }             System.out.print(p);         }     } }

---
+++ sum_of_elements_between_A_and_B.java
@@ -0,0 +1,37 @@
+import java.util.Scanner;
+class First
+{
+    public static void main(String[] args)
+    {
+        Scanner sc=new Scanner(System.in);
+        int a=sc.nextInt();
+        int[] arr=new int[a];
+        int[] arr2=new int[100];
+        int i,j,k=0,l=0,m=0,n=0,p=0;
+        for(i=0;i<a;i++)
+        {
+            arr[i]=sc.nextInt();
+        }
+        int b=sc.nextInt();
+        int c=sc.nextInt();
+        for(i=0;i<a;i++)
+        {
+            if(arr[i]>=b && arr[i]<=c)
+            {
+                arr2[m++]=arr[i];
+            }
+        }
+        if(m==0)
+        {
+            System.out.print("-1");
+        }
+        else
+        {
+            for(i=0;i<m;i++)
+            {
+                p+=arr2[i];
+            }
+            System.out.print(p);
+        }
+    }
+}
