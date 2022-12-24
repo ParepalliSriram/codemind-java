7e45ffbc05389d2ff72693c14c66266a92bdc20c
ParepalliSriram <96734287+ParepalliSriram@users.noreply.github.com>
2022-12-24 23:27:12 +0530

    import java.util.Scanner; class Main {     public static void main(String[] args)     {         Scanner sc=new Scanner(System.in);         int a=sc.nextInt();         int b=a;         for(int i=1;i<=a;i++)         {             for(int k=1;k<=a;k++)             {                 if(k==i || k==a-i+1)                 {                     System.out.print(b+" ");                 }                 else                 {                     System.out.print(" ");                 }             }             b-=1;             System.out.println();         }     } }

--- Print_the_following_pattern.java
+++ Print_the_following_pattern.java
@@ -5,12 +5,21 @@ class Main
     {
         Scanner sc=new Scanner(System.in);
         int a=sc.nextInt();
+        int b=a;
         for(int i=1;i<=a;i++)
         {
-            for(int k=i;k<=a;k++)
+            for(int k=1;k<=a;k++)
             {
-                System.out.print(k+" ");
+                if(k==i || k==a-i+1)
+                {
+                    System.out.print(b+" ");
+                }
+                else
+                {
+                    System.out.print(" ");
+                }
             }
+            b-=1;
             System.out.println();
         }
     }
